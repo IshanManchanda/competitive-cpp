5d02dfe10758a989e91e0af28d3f5234ec4a0a8d
Ishan Manchanda <ishanmanchanda70@gmail.com>
2020-01-11 11:25:14 +0530

    Rewrote solution to Free Ticket - INOIPRAC

--- INOIPRAC/Free Ticket - INOI1402.cpp
+++ INOIPRAC/Free Ticket - INOI1402.cpp
@@ -1,43 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-# define INF 100000001
+#define INF ((unsigned) ~0)
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int C, x, y;
-	long long F, p, m = 0;
-	cin >> C >> F;
+	long long n, f, x, y, p, ans = 0;
+	cin >> n >> f;
 
-	long long graph[C + 1][C + 1];
-	for (long long i = 0; i < C; i++)
-		for (long long j = 0; j < C; j++)
-			graph[i][j] = INF;
+	long long dist[n + 1][n + 1];
+	for (long long i = 0; i < n; ++i)
+		for (long long j = 0; j < n; ++j)
+			dist[i][j] = INF;
 
-	for (long long i = 0; i < C; i++)
-		graph[i][i] = 0;
+	for (long long i = 0; i < n; ++i)
+		dist[i][i] = 0;
 
-	for (long long i = 0; i < F; i++) {
+	for (int i = 0; i < f; ++i) {
 		cin >> x >> y >> p;
-		graph[x - 1][y - 1] = p;
-		graph[y - 1][x - 1] = p;
+		dist[x - 1][y - 1] = p;
+		dist[y - 1][x - 1] = p;
 	}
 
-	for (long long k = 0; k < C; k++)
-		for (long long i = 0; i < C; i++)
-			for (long long j = 0; j < C; j++)
-				if ((graph[i][k] != INF) && (graph[k][j] != INF))
-					if (graph[i][k] + graph[k][j] < graph[i][j])
-						graph[i][j] = graph[i][k] + graph[k][j];
+	for (long long k = 0; k < n; ++k)
+		for (long long i = 0; i < n; ++i)
+			for (long long j = 0; j < n; ++j)
+				if (dist[i][k] != INF and dist[k][j] != INF)
 
-	for (long long i = 0; i < C; i++)
-		for (long long j = 0; j < C; j++)
-			if (graph[i][j] != INF)
-				m = max(m, graph[i][j]);
+					if (dist[i][j] > dist[i][k] + dist[k][j])
+						dist[i][j] = dist[i][k] + dist[k][j];
 
-	cout << m << endl;
+	for (long long i = 0; i < n; ++i)
+		for (long long j = 0; j < i; ++j)
+			if (dist[i][j] != INF)
+				ans = max(ans, dist[i][j]);
 
+	cout << ans << endl;
 }
--- ZCOPRAC/Wormholes - ZCO12002.cpp
+++ ZCOPRAC/Wormholes - ZCO12002.cpp
@@ -22,9 +22,12 @@ int main() {
 	exam n[N + 1];
 	long long v[V + 1], w[W + 1];
 
-	for (long long i = 0; i < N; i++) { cin >> n[i].start >> n[i].end; }
-	for (long long i = 0; i < V; i++) { cin >> v[i]; }
-	for (long long i = 0; i < W; i++) { cin >> w[i]; }
+	for (long long i = 0; i < N; i++)
+		cin >> n[i].start >> n[i].end;
+	for (long long i = 0; i < V; i++)
+		cin >> v[i];
+	for (long long i = 0; i < W; i++)
+		cin >> w[i];
 
 	sort(n, n + N, compare);
 	sort(v, v + V);
@@ -32,51 +35,42 @@ int main() {
 
 	for (long long i = 0; i < N; i++) {
 		exam c = n[i];
-		if (v[0] > c.start) {
-			continue;
-		}
-		if (w[W - 1] < c.end) {
-			continue;
-		}
+		if (v[0] > c.start) continue;
+		if (w[W - 1] < c.end) continue;
 
-		if (v[V - 1] <= c.start) {
+		if (v[V - 1] <= c.start)
 			leave = v[V - 1];
-		}
 		else {
 			low = 0;
 			high = V - 1;
 			while (high > low) {
 				mid = (low + high) / 2;
-				if (v[mid] > c.start) {
+
+				if (v[mid] > c.start)
 					high = mid;
-				} else {
-					low = mid + 1;
-				}
+				else low = mid + 1;
 			}
 			leave = v[low - 1];
 		}
 
-		if (w[0] >= c.end) {
+		if (w[0] >= c.end)
 			reach = w[0];
-		}
 		else {
 			low = 0;
 			high = W - 1;
 			while (high > low) {
 				mid = (low + high) / 2;
-				if (w[mid] > c.end) {
+				if (w[mid] > c.end)
 					high = mid;
-				} else {
-					low = mid + 1;
-				}
+				else low = mid + 1;
 			}
-			if (w[high - 1] < c.end) { reach = w[high]; }
-			else { reach = w[high - 1]; }
+			if (w[high - 1] < c.end)
+				reach = w[high];
+			else reach = w[high - 1];
 		}
 
-		if ((reach - leave + 1) < m) {
+		if ((reach - leave + 1) < m)
 			m = reach - leave + 1;
-		}
 	}
 	cout << m << endl;
 }
