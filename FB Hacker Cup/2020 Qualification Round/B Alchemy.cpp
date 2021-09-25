7839735fbc4f8c66a53bb4dfaedeb6593e857214
Ishan Manchanda <ishanmanchanda70@gmail.com>
2021-09-25 14:43:14 +0530

    Prac FB Hackercup 2020 qual round

---
+++ FB Hacker Cup/2020 Qualification Round/B Alchemy.cpp
@@ -0,0 +1,58 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma,tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case #" << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+//#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define NEWL "\n";
+
+typedef long long ll;
+//typedef __int128_t lll;
+//typedef long double ld;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vi> vvl;
+
+
+int main() {
+	FAST_IO
+	FILE_IN
+	FILE_OUT
+
+	TESTCASES1 {
+		int n, a = 0;
+		string s;
+		cin >> n >> s;
+		REP(i, 0, n) a += s[i] == 'A';
+		CASEOUT << ((a == n / 2 || a == n / 2 + 1) ? "Y\n" : "N\n");
+	}
+
+	cout << flush;
+}
---
+++ FB Hacker Cup/2020 Qualification Round/D1 Running on Fumes Chapter 1.cpp
@@ -0,0 +1,71 @@
+//#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma,tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case #" << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+//#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define NEWL "\n";
+
+typedef long long ll;
+//typedef __int128_t lll;
+//typedef long double ld;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vi> vvl;
+
+
+int main() {
+	FAST_IO
+	FILE_IN
+	FILE_OUT
+
+	TESTCASES1 {
+		int n, m;
+		cin >> n >> m;
+		ll c[n];
+		REP(i, 0, n) cin >> c[i];
+
+		deque<pair<int, ll>> q;
+		q.emplace_back(0, 0);
+		bool flag = false;
+		REP(i, 1, n) {
+			while (!q.empty() && q.front().F < i - m) q.pop_front();
+			if (q.empty()) {flag = true; break;}
+
+			if (!c[i]) continue;
+			ll cost = c[i] + q.front().S;
+			while (!q.empty() && q.back().S >= cost) q.pop_back();
+			q.emplace_back(i, cost);
+		}
+		CASEOUT << (flag ? -1 : q.front().S) << "\n";
+	}
+
+	cout << flush;
+}
