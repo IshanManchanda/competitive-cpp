ded6a615171ef4d2ec27b63d98fafad13e68b904
Ishan Manchanda <ishanmanchanda70@gmail.com>
2021-09-26 13:53:25 +0530

    Some template optimizations (pbds)

--- template.cpp
+++ template.cpp
@@ -1,10 +1,8 @@
 //#pragma GCC optimize("O3,unroll-loops")
-//#pragma GCC target("avx,avx2,fma,tune=native")
+#pragma GCC target("avx,avx2,fma,tune=native")
 
 #include <bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 
 #define F first
 #define S second
@@ -15,8 +13,8 @@ using namespace __gnu_pbds;
 #define MP make_pair
 #define MT make_tuple
 
-#define REP(i,a,b) for (ll i = (a); i < (b); ++i)
-#define RREP(i,a,b) for (ll i = (a); i > (b); --i)
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
 
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #define FILE_IN freopen("input.txt", "r", stdin);
@@ -32,23 +30,28 @@ using namespace __gnu_pbds;
 //#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
 #define NEWL "\n";
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-	tree_order_statistics_node_update> indexed_set;
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//	tree_order_statistics_node_update> indexed_set;
+/* find_by_order(k) and order_of_key(x) */
 
 typedef long long ll;
 //typedef __int128_t lll;
 //typedef long double ld;
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
 typedef tuple<int, int, int> ti;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<ll> vl;
 typedef vector<vi> vvl;
 
+
 int main() {
 	FAST_IO
-	FILE_IN
-	FILE_OUT
+//	FILE_IN
+//	FILE_OUT
 
 	TESTCASES {
 
