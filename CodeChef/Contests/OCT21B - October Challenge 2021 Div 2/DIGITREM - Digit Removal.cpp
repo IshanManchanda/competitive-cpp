af61f7fbc6783c3c499750e1646d821b5c8b1aec
Ishan Manchanda <ishanmanchanda70@gmail.com>
2021-10-02 12:06:57 +0530

    Added Oct Challenge Digit Removal

---
+++ CodeChef/Contests/OCT21B - October Challenge 2021 Div 2/DIGITREM - Digit Removal.cpp
@@ -0,0 +1,80 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma,tune=native")
+#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case #" << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+//#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define NEWL "\n";
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//	tree_order_statistics_node_update> indexed_set;
+/* find_by_order(k) and order_of_key(x) */
+
+typedef long long ll;
+//typedef __int128_t lll;
+//typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vi> vvl;
+
+
+int main() {
+	FAST_IO
+	FILE_IN
+	FILE_OUT
+
+	TESTCASES {
+		ll n, d, x = 1e9, ans;
+		cin >> n >> d;
+
+		while (x > 0 && (x > n || (n / x) % 10 != d)) x /= 10;
+		if (x == 0) {
+			cout << "0\n";
+			continue;
+		}
+
+		ans = (((n / x) % 10 + 1) * x) - (n % (10 * x));
+		if (d == 0) while (x > 0) ans += x /= 10;
+
+		if (d == 9) {
+			x *= 10;
+			while ((n / x) % 10 == 8) x *= 10;
+			ans = x - (n % x);
+		}
+
+		cout << ans << "\n";
+	}
+
+	cout << flush;
+}
