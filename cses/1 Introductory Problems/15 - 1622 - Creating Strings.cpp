333aa2c650d6ad558a2dbf74769a54333680afdb
Ishan Manchanda <ishanmanchanda70@gmail.com>
2021-10-10 16:14:25 +0530

    Added several CSES Introductory Problems

---
+++ cses/1 Introductory Problems/15 - 1622 - Creating Strings.cpp
@@ -0,0 +1,70 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma,tune=native")
+#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case #" << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define NEWL "\n";
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//	tree_order_statistics_node_update> indexed_set;
+/* find_by_order(k) and order_of_key(x) */
+
+typedef long long ll;
+//typedef __int128_t lll;
+//typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vi> vvl;
+
+int main() {
+	FAST_IO
+//	FILE_IN
+//	FILE_OUT
+
+//	TESTCASES {
+	string s;
+	cin >> s;
+
+	sort(all(s));
+	vector<string> sv;
+	do sv.PB(s);
+	while (next_permutation(all(s)));
+
+	cout << sv.size() << "\n";
+	for (string s1 : sv) cout << s1 << "\n";
+//	}
+
+	cout << flush;
+}
---
+++ cses/1 Introductory Problems/16 - 1623 - Apple Division.cpp
@@ -0,0 +1,83 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma,tune=native")
+#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+
+#define cinai(a, n) REP(ii, 0, (n)) cin >> (a)[ii];
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case #" << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define NEWL "\n";
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//	tree_order_statistics_node_update> indexed_set;
+/* find_by_order(k) and order_of_key(x) */
+
+typedef long long ll;
+//typedef __int128_t lll;
+//typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vi> vvl;
+
+int main() {
+	FAST_IO
+//	FILE_IN
+//	FILE_OUT
+
+//	TESTCASES {
+	int n;
+	cin >> n;
+	ll a[n], s;
+	cinai(a, n);
+	ll ans = s = accumulate(a, a+n, 0LL);
+	// Hypothesis: Sorting improves speed by increasing chances of early break
+//	sort(a, a + n, greater<>());
+
+	for (ll b = 0; b < (1LL << n); b++) {
+		ll s1 = 0;
+		REP(i, 0, n) {
+			if (b & (1 << i)) {
+				s1 += a[i];
+			}
+			if (s1 > s) break;
+		}
+		ans = min(ans, abs(s - 2 * s1));
+		if (ans == 0) break;
+	}
+	cout << ans;
+
+//	}
+
+	cout << flush;
+}
