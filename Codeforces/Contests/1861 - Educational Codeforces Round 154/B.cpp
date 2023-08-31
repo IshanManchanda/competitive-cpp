d423fec6641e4525ce52c5a49517269497276607
Ishan Manchanda <ishanmanchanda70@gmail.com>
2023-08-31 22:59:27 +0530

    added some problems from ladder id21, added attempt for CF Ed round 154 with D upsolve

---
+++ Codeforces/Contests/1861 - Educational Codeforces Round 154/B.cpp
@@ -0,0 +1,105 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma,tune=native")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC target("tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+#define cinai(a, n) REP(i, 0, n) cin >> a[i]
+#define cinani(a, n) int n; cin >> n; int a[n]; cinai(a, n)
+#define cinan(a, n) ll n; cin >> n; ll a[n]; cinai(a, n)
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case " << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define endl "\n";
+
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ") ";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "{ "; for (auto& it : p) os << it << " "; return os << "} ";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "{ "; for (auto& it : p) os << it << " "; return os << "} ";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "{{ "; for (auto& it : p) os << it << " "; return os << "}} ";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//	tree_order_statistics_node_update> indexed_set;
+/* find_by_order(k) and order_of_key(x) */
+
+typedef long long ll;
+typedef unsigned long long ull;
+//typedef __int128_t lll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+
+int main() {
+    FAST_IO
+//	FILE_IN
+//	FILE_OUT
+//	cout << setprecision(11);
+
+    TESTCASES {
+        string a, b;
+        cin >> a >> b;
+        int n = a.size();
+        if ((a[1] == '1' && b[1] == '1') || (a[n - 1] == '0' && b[n - 1] == '0')) {
+            cout << "YES\n";
+            continue;
+        }
+        int flag = 1;
+        for (int i = 1; i < n; i++) {
+            if (a[i] == '1' && b[i] == '1') {
+                cout << "YES\n";
+                flag = 0;
+                break;
+            }
+            if (a[i] == '0' && b[i] == '0') continue;
+            if (a[i] == '0') swap(a, b);
+            // a[i] is 1 and b[i] is 0.
+            // find the first 0 in a and switch
+            int j = i + 1;
+            for (; j < n && a[j] == '1'; j++);
+            if (j == n) break;
+            for (int k = i; k < j; k++) a[k] = '0';
+        }
+        if (flag) cout << "NO\n";
+    }
+
+    cout << flush;
+}
