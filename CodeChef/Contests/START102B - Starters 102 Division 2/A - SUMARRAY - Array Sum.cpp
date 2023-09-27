f46ab159ae5fba47417e3fa1d767361e38428e73
Ishan Manchanda <ishanmanchanda70@gmail.com>
2023-09-27 22:05:11 +0530

    Added attempt for CC START102B

---
+++ CodeChef/Contests/START102B - Starters 102 Division 2/A - SUMARRAY - Array Sum.cpp
@@ -0,0 +1,154 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma,tune=native")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC target("tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+#define cinai(a, n) REP(ii, 0, n) cin >> a[ii]
+#define cinani(a, n) int n; cin >> n; int a[n]; cinai(a, n)
+#define cinan(a, n) ll n; cin >> n; ll a[n]; cinai(a, n)
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case #" << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define endl "\n";
+
+#ifdef ISHAN_LOCAL
+#define deb(x)             \
+    {                      \
+        cerr << #x << " "; \
+        _print(x);         \
+        cerr << "\n";      \
+    }
+#else
+#define deb(x)
+#endif
+
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ") ";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "{ "; for (auto& it : p) os << it << " "; return os << "} ";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "{ "; for (auto& it : p) os << it << " "; return os << "} ";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "{{ "; for (auto& it : p) os << it << " "; return os << "}} ";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//	tree_order_statistics_node_update> indexed_set;
+/* find_by_order(k) and order_of_key(x) */
+
+typedef long long ll;
+typedef unsigned long long ull;
+//typedef __int128_t lll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+// x^y mod m in O(log y)
+ll bin_exp_mod(ll x, ll y, ll m) {
+    x %= m;
+    ll ans = 1LL;
+    while (y) {
+        if (y & 1) ans = (ans * x) % m;
+        x = (x * x) % m;
+        y >>= 1;
+    }
+    return ans;
+}
+// Works when m is prime, use extended GCD otherwise
+inline ll mod_inv(ll x, ll m) {
+    return bin_exp_mod(x, m - 2, m);
+}
+
+
+int main() {
+    FAST_IO
+//	FILE_IN
+//	FILE_OUT
+//	cout << setprecision(11);
+
+    TESTCASES {
+        ll n, k;
+        cin >> n >> k;
+        ll n1 = n/2;
+        ll min1 = n1 * 3;
+        ll max1 = (100000 + 99999) * n1;
+        if (k < min1 || k > max1) {
+            cout << "-1\n";
+            continue;
+        }
+        if (n1 % 2 == 0) {
+            // even number of both, can only make even sum
+            if (k % 2 == 1)  {
+                cout << "-1\n";
+                continue;
+            }
+            // K IS EVEN AND N/2 IS EVEN
+            // generate n avg and add 1 to all in extra
+            // if this is less than n/2, we reduce
+            ll avg = k / n, extra = k % n, red = 0, dub = 0;
+            // extra between n1 and n
+            if (extra > n1) dub = (extra - n1)/2, extra = n1;
+            red = (n1 - extra) / 2;
+            REP(i, 0, red) cout << avg - 1 << " ";
+            REP(i, 0, extra + red) cout << avg + 1 << " ";
+            REP(i, 0, dub) cout << avg + 2 << " ";
+            REP(i, 0, n1 - dub) cout << avg << " ";
+            cout << endl;
+        }
+        else {
+            // odd number of both, can only make odd sum
+            if (k % 2 == 0)  {
+                cout << "-1\n";
+                continue;
+            }
+            // K IS ODD AND N/2 IS ODD
+            ll avg = k / n, extra = k % n, red = 0, dub = 0;
+            // extra between n1 and n
+            if (extra > n1) dub = (extra - n1)/2, extra = n1;
+            red = (n1 - extra) / 2;
+            REP(i, 0, red) cout << avg - 1 << " ";
+            REP(i, 0, extra + red) cout << avg + 1 << " ";
+            REP(i, 0, dub) cout << avg + 2 << " ";
+            REP(i, 0, n1 - dub) cout << avg << " ";
+            cout << endl;
+        }
+    }
+
+    cout << flush;
+}
---
+++ CodeChef/Contests/START102B - Starters 102 Division 2/E - CHSEG - Chef Segment.cpp
@@ -0,0 +1,149 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma,tune=native")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC target("tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define PB emplace_back
+#define MP make_pair
+#define MT make_tuple
+
+#define REP(i, a, b) for (ll i = (a); i < (b); ++i)
+#define RREP(i, a, b) for (ll i = (a); i > (b); --i)
+#define cinai(a, n) REP(ii, 0, n) cin >> a[ii]
+#define cinani(a, n) int n; cin >> n; int a[n]; cinai(a, n)
+#define cinan(a, n) ll n; cin >> n; ll a[n]; cinai(a, n)
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define FILE_IN freopen("input.txt", "r", stdin);
+#define FILE_OUT freopen("output.txt", "w", stdout);
+
+#define TESTCASES ll tt; cin >> tt; while (tt--)
+#define TESTCASES1 ll tt; cin >> tt; REP(ttt, 1, tt + 1)
+#define CASEOUT cout << "Case #" << ttt << ": "
+
+#define NEG_INF (-LLONG_MAX)
+#define FLOAT_EQ(a, b) (abs((a) - (b)) < 1e-9)
+#define MOD (1'000'000'007)
+//#define MOD_SUM(a, b) ((a) + (b) >= MOD) ? ((a) + (b) - MOD) : ((a) + (b))
+#define endl "\n";
+
+#ifdef ISHAN_LOCAL
+#define deb(x)             \
+    {                      \
+        cerr << #x << " "; \
+        _print(x);         \
+        cerr << "\n";      \
+    }
+#else
+#define deb(x)
+#endif
+
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ") ";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "{ "; for (auto& it : p) os << it << " "; return os << "} ";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "{ "; for (auto& it : p) os << it << " "; return os << "} ";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "{{ "; for (auto& it : p) os << it << " "; return os << "}} ";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "] ";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//	tree_order_statistics_node_update> indexed_set;
+/* find_by_order(k) and order_of_key(x) */
+
+typedef long long ll;
+typedef unsigned long long ull;
+//typedef __int128_t lll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+// x^y mod m in O(log y)
+ll bin_exp_mod(ll x, ll y, ll m) {
+    x %= m;
+    ll ans = 1LL;
+    while (y) {
+        if (y & 1) ans = (ans * x) % m;
+        x = (x * x) % m;
+        y >>= 1;
+    }
+    return ans;
+}
+// Works when m is prime, use extended GCD otherwise
+inline ll mod_inv(ll x, ll m) {
+    return bin_exp_mod(x, m - 2, m);
+}
+
+
+int main() {
+    FAST_IO
+//	FILE_IN
+//	FILE_OUT
+//	cout << setprecision(11);
+
+    TESTCASES {
+        ll n;
+        cin >> n;
+        string s1, s2;
+        cin >> s1 >> s2;
+        int x[n];
+        REP(i, 0, n) {
+            x[i] = (s1[i] - '0') ^ (s2[i] - '0');
+        }
+        ll a = 0, b = 0, c = 0, d = 0, e = 0;
+        ll i = 0;
+        while (i < n && x[i] == 0) i++, a++;
+        while (i < n && x[i] != 0) i++, b++;
+        while (i < n && x[i] == 0) i++, c++;
+        while (i < n && x[i] != 0) i++, d++;
+        while (i < n && x[i] == 0) i++, e++;
+//        cerr << a << " " << b << " " << c << " " << d << " " << e << endl;
+        if (i != n) {
+            cout << "0\n";
+            continue;
+        }
+        if (d != 0 || e != 0) {
+            // CASE 2
+            cout << 6 << endl;
+        }
+        else if (b == 0 && c == 0) {
+            // CASE 0
+            // no diffs. Basically do nC2 possible flip and then revert it
+            cout << (n * (n + 1)) / 2 << endl;
+        }
+        else {
+            // CASE 1
+            // a 0s then b 1s then c 0s
+            // flip all bs and also some a or some c
+            // and in second, flip them back
+            // only b not allowed
+            // thus a - 1 values here and b - 1 values here
+            // OR do center only by partially
+            // min 1 and max b - 1 => b - 1 ways
+            cout << 2 * (a + c + b - 1) << endl;
+        }
+    }
+
+    cout << flush;
+}
